93a05ff00cca08e371a26a6e92d26880a5986bee
kuper0201 <kuper171@gmail.com>
2022-10-04 00:03:27 +0900

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -40,7 +40,7 @@ sudo chmod 777 ~/.config/polybar/scripts/*
 
 echo "========================================================================="
 echo "getting the gtk Theme"
-sudo git clone https://github.com/jmattheis/gruvbox-dark-gtk ~/usr/share/themes/gruvbox-theme 
+sudo git clone https://github.com/kuper0201/gruvbox-dark-gtk /usr/share/themes/gruvbox-theme 
 echo "========================================================================="
 echo "getting nerd JetBrainsMono Nerd Fonts"
 curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
